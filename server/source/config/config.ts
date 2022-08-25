6d623e3e919f41b6d0b6e3398d378264e14cfa5c
Joe <joe@hungrysandwich.club>
2022-08-25 20:13:30 +0100

    Add mysql

--- server/source/config/config.ts
+++ server/source/config/config.ts
@@ -2,6 +2,20 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const MYSQL_HOST = process.env.MYSQL_HOST || 'localhost';
+const MYSQL_PORT = Number(process.env.MYSQL_PORT) || 0;
+const MYSQL_DATABASE = process.env.MYSQL_DATABASE || '';
+const MYSQL_USER = process.env.MYSQL_USER || '';
+const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD || '';
+
+const MYSQL = {
+  host: MYSQL_HOST,
+  port: MYSQL_PORT,
+  database: MYSQL_DATABASE,
+  user: MYSQL_USER,
+  password: MYSQL_PASSWORD,
+}
+
 const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || 'localhost';
 const SERVER_PORT = process.env.SERVER_PORT || 3333;
 
@@ -11,7 +25,8 @@ const SERVER = {
 }
 
 const config = {
-  server: SERVER
+  mysql: MYSQL,
+  server: SERVER,
 }
 
 export default config;
